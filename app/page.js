31b5424a82010e4c9664908394799fcec5dc6049
nataliakorvoshky-coder <nataliakorvoshky@gmail.com>
2026-03-07 16:48:44 -0700

    redirect homepage to login

--- app/page.js
+++ app/page.js
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <main>
-      <h1>My First Website 🚀</h1>
-      <p>It works!</p>
-    </main>
-  );
+  redirect("/login")
 }
